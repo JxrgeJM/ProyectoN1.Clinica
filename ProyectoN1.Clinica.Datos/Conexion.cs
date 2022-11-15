fc16271a3cd4c74f80abd11d483002ad81248423
JORGE JIMENEZ MELENDEZ <jorgejm@outlook.com>
2022-11-15 11:35:04 -0600

    Se crean las clases de acceso a datos (DAL)

---
+++ ProyectoN1.Clinica.Datos/Conexion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace ProyectoN1.Clinica.Datos
+{
+    public class Conexion
+    {
+        private static SqlConnection _cnxClinica = null;
+        public static SqlConnection getCnxClinica()
+        {
+            if (_cnxClinica == null)
+                _cnxClinica = new SqlConnection(ConfigurationManager.ConnectionStrings["CnxClinica"].ConnectionString);
+            return _cnxClinica;
+        }
+    }
+}

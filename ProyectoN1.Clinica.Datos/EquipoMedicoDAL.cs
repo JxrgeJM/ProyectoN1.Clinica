1caf19ffd34523e8d54341254eb232059f0ecf3c
JORGE JIMENEZ MELENDEZ <jorgejm@outlook.com>
2022-12-06 10:19:23 -0600

    Proyecto N2 - Se agrega clase EquipoMedico y EquipoMedicoDAL

---
+++ ProyectoN1.Clinica.Datos/EquipoMedicoDAL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using ProyectoN1.Clinica.Entidades;
+
+namespace ProyectoN1.Clinica.Datos
+{
+    public class EquipoMedicoDAL
+    {
+
+    }
+}
---
+++ ProyectoN1.Clinica.Entidades/EquipoMedico.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoN1.Clinica.Entidades
+{
+    public class EquipoMedico
+    {
+        public EquipoMedico()
+        {
+            Activo = "";
+            Nombre = "";
+            Serie = "";
+            Descripcion = "";
+            FechaCompra = DateTime.Now;
+            Especialidad = new TipoEspecialidad();
+            Clinica = new Clinica();
+        }
+
+        public EquipoMedico(string activo, string nombre, string serie, string descripcion, 
+            DateTime fechaCompra, TipoEspecialidad especialidad, Clinica clinica)
+        {
+            Activo = activo;
+            Nombre = nombre;
+            Serie = serie;
+            Descripcion = descripcion;
+            FechaCompra = fechaCompra;
+            Especialidad = especialidad;
+            Clinica = clinica;
+        }
+
+        [Required(ErrorMessage = "Requerido")]
+        public string Activo { get; set; }
+
+        [Required(ErrorMessage = "Requerido")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Requerido")]
+        public string Serie { get; set; }
+
+        [Required(ErrorMessage = "Requerido")]
+        public string Descripcion { get; set; }
+
+        [Required(ErrorMessage = "Requerido")]
+        public DateTime FechaCompra { get; set; }
+
+        public TipoEspecialidad Especialidad { get; set; }
+
+        public Clinica Clinica { get; set; }
+
+    }
+}

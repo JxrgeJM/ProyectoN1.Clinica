3f526e1d4d21b1f615dbe45aaf264119c6bbf930
JORGE JIMENEZ MELENDEZ <jorgejm@outlook.com>
2022-12-06 10:32:57 -0600

    Se agregan los metodos de acceso a datos para EquipoMedico

--- ProyectoN1.Clinica.Datos/EquipoMedicoDAL.cs
+++ ProyectoN1.Clinica.Datos/EquipoMedicoDAL.cs
@@ -11,6 +11,57 @@ namespace ProyectoN1.Clinica.Datos
 {
     public class EquipoMedicoDAL
     {
+        public static List<EquipoMedico> Listar()
+        {
+            SqlCommand vCmd = new SqlCommand("SP_ListarEquipoMedico", Conexion.getCnxClinica());
+            vCmd.CommandType = CommandType.StoredProcedure;
+            try
+            {
+                Conexion.getCnxClinica().Open();
+                SqlDataReader vRd = vCmd.ExecuteReader();
+                List<EquipoMedico> vDatos = new List<EquipoMedico>();
+                while (vRd.Read())
+                    vDatos.Add(new EquipoMedico(vRd.GetString(0), vRd.GetString(1), vRd.GetString(2), vRd.GetString(3), vRd.GetDateTime(4), 
+                        new TipoEspecialidad(), 
+                        new Entidades.Clinica()));
+                vRd.Close();
+                return vDatos;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                Conexion.getCnxClinica().Close();
+            }
+        }
 
+        public static void AgregarEquipoMedico(EquipoMedico pEquipoMedico)
+        {
+            SqlCommand vCmd = new SqlCommand("SP_AgregarEquipoMedico", Conexion.getCnxClinica());
+            vCmd.CommandType = CommandType.StoredProcedure;
+            vCmd.Parameters.Clear();
+            vCmd.Parameters.AddWithValue("@pNumActivo", pEquipoMedico.Activo);
+            vCmd.Parameters.AddWithValue("@pNombre", pEquipoMedico.Nombre);
+            vCmd.Parameters.AddWithValue("@pSerie", pEquipoMedico.Serie);
+            vCmd.Parameters.AddWithValue("@pDescripcion", pEquipoMedico.Descripcion);
+            vCmd.Parameters.AddWithValue("@pFechaCompra", pEquipoMedico.FechaCompra);
+            vCmd.Parameters.AddWithValue("@pIdEspecialidad", pEquipoMedico.Especialidad.Id);
+            vCmd.Parameters.AddWithValue("@pIdClinica", pEquipoMedico.Clinica.Id);
+            try
+            {
+                Conexion.getCnxClinica().Open();
+                vCmd.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                Conexion.getCnxClinica().Close();
+            }
+        }
     }
 }
